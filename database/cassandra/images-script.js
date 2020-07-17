a3609b891286cb3081e9982bf198d88823198912
emvh <emilyvho@gmail.com>
2020-07-17 13:31:06 -0700

    Fix script to make address consistent per listing id

--- database/cassandra/images-script.js
+++ database/cassandra/images-script.js
@@ -2,46 +2,51 @@ const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const faker = require('faker');
 
-const writeImages = fs.createWriteStream('./database/cassandra/images-cassandra.csv');
-writeImages.write('image_id,listing_id,address,city,state,zip_code,price,image_url,display_order\n','utf8');
+const writeImages = fs.createWriteStream('./database/cassandra/images-cql1.csv');
+writeImages.write('image_id,listing_id,address,city,state,zip_code,price,image_url,display_order\n','utf-8');
 
 const imagesDataGen = (writer, encoding, callback) => {
-  let targetNumRecords = 60000000;
+  // let targetNumRecords = 50000000;
   let image_id = 0;
-  let listing_id = 0;
+  let listing_id = 1;
   const photoIndex = () => faker.random.number({min: 1, max: 35});
   const randomNum = () => faker.random.number({min: 5, max: 7});
+  const addressGenerator = () => faker.address.streetAddress();
+  const cityGenerator = () => faker.address.city();
+  const stateGenerator = () => faker.address.stateAbbr();
+  const zip_codeGenerator = () => faker.address.zipCode('#####');
+  const priceGenerator = () => faker.random.number({min: 1000, max: 7000}) * 1000;
 
   const write = () => {
     let ok = true;
-    while (image_id <= targetNumRecords && ok) {
+    while (listing_id <= 10000000 && ok) {
       let photoCounter = 1;
       let totalPhotos = randomNum();
-      listing_id++;
+      const address = addressGenerator();
+      const city = cityGenerator();
+      const state = stateGenerator();
+      const zip_code = zip_codeGenerator();
+      const price = priceGenerator();
+
       while (photoCounter <= totalPhotos) {
         image_id++;
-        const address = faker.address.streetAddress();
-        const city = faker.address.city();
-        const state = faker.address.stateAbbr();
-        const zip_code = faker.address.zipCode('#####');
-        const price = faker.random.number({min: 1000, max: 7000}) * 1000;
-        const image_url = `https://sdc2.s3-us-west-1.amazonaws.com/photo${photoIndex()}.jpg`;
+        const image_url = `https://sdc4.s3-us-west-1.amazonaws.com/p${photoIndex()}.jpg`;
         const display_order = photoCounter;
         const data = `${image_id},${listing_id},${address},${city},${state},${zip_code},${price},${image_url},${display_order}\n`;
-        if (targetNumRecords === 0) {
+        if (listing_id === 10000000) {
           writer.write(data, encoding, callback);
         } else {
           ok = writer.write(data, encoding);
         }
-        if (image_id % 10000000 === 0) {
+        if (listing_id % 10000000 === 0) {
           console.log('write ', image_id);
         };
         photoCounter++;
       }
-      targetNumRecords--;
+      listing_id++;
     }
-    if (targetNumRecords > 0) {
-      writeImages.once('drain', write);
+    if (listing_id <= 10000000) {
+      writer.once('drain', write);
     }
   }
   write();
