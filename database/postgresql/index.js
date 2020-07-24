f6478c5e7cb5fc2a38dc6c33efed42cd60ab3f78
emvh <emilyvho@gmail.com>
2020-07-23 22:45:23 -0700

    Fix file

--- database/postgresql/index.js
+++ database/postgresql/index.js
@@ -1,11 +1,7 @@
 const { Client } = require('pg');
 const { psql_username, psql_password } = require('./config/psql.login.js');
 
-<<<<<<< HEAD
-const host = '54.153.112.42:5432';
-=======
 const host = '54.153.112.42';
->>>>>>> 8f135484fd9586f4a8ecc0de96863c54b2fe3a61
 const database = 'home_listings';
 
 const connectStr = `postgres://${psql_username}:${psql_password}@${host}/${database}`;
